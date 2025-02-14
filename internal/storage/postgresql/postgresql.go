21ae0eaa859eb673a59da9e65daf949eb100f3f0
Killazius <migunovmaksim44@gmail.com>
2025-02-14 23:31:38 +0300

    added gorm instead of pure SQL

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -1,46 +1,31 @@
 package postgresql
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
-	_ "github.com/jackc/pgx/v5/stdlib" // driver postgresql
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"shorturl/internal/storage"
 	"time"
 )
 
 type Storage struct {
-	db *sql.DB
+	db *gorm.DB
+}
+type URL struct {
+	ID        uint      `gorm:"primaryKey"`
+	Alias     string    `gorm:"unique;not null"`
+	URL       string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"not null;default:now()"`
 }
 
 func NewStorage(url string) (*Storage, error) {
 	const op = "storage.postgresql.NewStorage"
-	db, err := sql.Open("pgx", url)
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{}) //sql.Open("pgx", url)
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
-	stmt, err := db.Prepare(`CREATE TABLE IF NOT EXISTS url (
-    id SERIAL PRIMARY KEY,
-    alias TEXT NOT NULL UNIQUE,
-    url TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-	);`)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec()
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
-	}
-	stmt, err = db.Prepare(`CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);`)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec()
+	err = db.AutoMigrate(&URL{})
 	if err != nil {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
@@ -49,62 +34,42 @@ func NewStorage(url string) (*Storage, error) {
 
 func (s *Storage) SaveURL(urlToSave string, alias string, createdAt time.Time) error {
 	const op = "storage.postgresql.SaveURL"
-	stmt, err := s.db.Prepare(`INSERT INTO url(alias, url, created_at) VALUES ($1, $2, $3)`)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
+	url := URL{
+		Alias:     alias,
+		URL:       urlToSave,
+		CreatedAt: createdAt,
 	}
-	defer stmt.Close()
-	_, err = stmt.Exec(alias, urlToSave, createdAt)
-	if err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+	result := s.db.Create(&url)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
 			return storage.ErrURLExists
 		}
-		return fmt.Errorf("%s: %w", op, err)
+		return fmt.Errorf("%s: %w", op, result.Error)
 	}
 	return nil
 }
 
 func (s *Storage) GetURL(alias string) (string, error) {
 	const op = "storage.postgresql.GetURL"
-
-	stmt, err := s.db.Prepare(`SELECT url FROM url WHERE alias = $1`)
-	if err != nil {
-		return "", fmt.Errorf("%s: %w", op, err)
-	}
-	defer stmt.Close()
-
-	var url string
-	err = stmt.QueryRow(alias).Scan(&url)
-
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
+	var url URL
+	result := s.db.Where("alias = ?", alias).First(&url)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 			return "", storage.ErrURLNotFound
 		}
-		return "", fmt.Errorf("%s: %w", op, err)
+		return "", fmt.Errorf("%s: %w", op, result.Error)
 	}
-	return url, nil
+	return url.URL, nil
 }
 
 func (s *Storage) DeleteURL(alias string) error {
 	const op = "storage.postgresql.DeleteURL"
-
-	stmt, err := s.db.Prepare(`DELETE FROM url WHERE alias = $1`)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
+	result := s.db.Where("alias = ?", alias).Delete(&URL{})
+	if result.Error != nil {
+		return fmt.Errorf("%s: %w", op, result.Error)
 	}
-	defer stmt.Close()
-	result, err := stmt.Exec(alias)
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return fmt.Errorf("%s: %w", op, err)
-	}
-	if rowsAffected == 0 {
+	if result.RowsAffected == 0 {
 		return storage.ErrURLNotFound
 	}
-
 	return nil
 }
